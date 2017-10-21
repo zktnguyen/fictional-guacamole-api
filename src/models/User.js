1d7b7107fba350f175e3cade0207b23f78aa673c
Kim Nguyen <zktnguyen@ucdavis.edu>
2017-10-21 15:45:07 -0700

    Reset password, forgot password, validate token requests created, insecure reset password creation

--- src/models/User.js
+++ src/models/User.js
@@ -7,7 +7,8 @@ import uniqueValidator from 'mongoose-unique-validator';
 const schema = new mongoose.Schema({
   email: { type: String, required: true, lowercase: true, index: true, unique: true },
   passwordHash: { type: String, required: true },
-  confirmed: { type: Boolean, default: false }
+  confirmed: { type: Boolean, default: false },
+  confirmationToken: { type: String, default: "" }
 }, { timestamps: true });
 
 schema.methods.isValidPassword = function isValidPassword(password) {
@@ -18,10 +19,29 @@ schema.methods.setPassword = function setPassword(password) {
   this.passwordHash = bcrypt.hashSync(password, 10);
 };
 
+schema.methods.setConfirmationToken = function setConfirmationToken() {
+  this.confirmationToken = this.generateJWT();
+};
+
+schema.methods.generateConfirmationURL = function generateConfirmationURL() {
+  return `${process.env.HOST}/confirmation/${this.confirmationToken}`;
+};
+
+schema.methods.generateResetPasswordURL = function generateResetPasswordURL() {
+  return `${process.env.HOST}/reset_password/${this.generateResetPasswordToken()}`;
+}
+
 schema.methods.generateJWT = function generateJWT() {
   return jwt.sign({
-    email: this.email
-  }, 'secretkey');
+    email: this.email,
+    confirmed: this.confirmed
+  }, process.env.JWT_SECRET);
+};
+
+schema.methods.generateResetPasswordToken = function generateResetPasswordToken() {
+  return jwt.sign({
+    _id: this._id
+  }, process.env.JWT_SECRET, { expiresIn: "1h" });
 };
 
 schema.methods.toAuthJSON = function toAuthJSON() {
